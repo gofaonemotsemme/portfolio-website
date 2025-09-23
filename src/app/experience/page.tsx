e2c08d49732d362e68915e7bd0f9b3721342411c
gofaone ♡ <gofamotsemme@gmail.com>
2025-09-23 13:06:43 +0100

    done with exp
    the allergex is knocking me out, okes. i unfortunately will be missing the rapture.

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -13,37 +13,45 @@ const Experience = () => {
       <div className="row">
         <div className="col">
           <div className="experience-item">
+            <h3 className="experience-role">FRONTEND DEVELOPER</h3>
+            <p className="experience-company">BOS Technology</p>
+            <p className="experience-dates">Feb, 2024 - Feb, 2025</p>
+            <ul className="experience-responsibilities">
+              <li>Spearheaded user-facing feature development for a crypto platform.</li>
+              <li>Integrated blockchain functionalities using Web3.js and ethers.js.</li>
+              <li>Designed and developed user-centric features like wallet integrations.</li>
+              <li>Collaborated with UI/UX teams to implement design systems and optimize interfaces.</li>
+              <li>Led the integration of blockchain transaction functionalities.</li>
+              <li>Contributed to app performance optimization, reducing load times.</li>
+            </ul>
+          </div>
+        </div>
+        <div className="col">
+          <div className="experience-item">
+            <h3 className="experience-role">JUNIOR DEVELOPER</h3>
+            <p className="experience-company">WeThinkCode_</p>
+            <p className="experience-dates">Jan, 2023 - Jan, 2024</p>
+            <ul className="experience-responsibilities">
+              <li>Collaborated with teams to develop user-friendly, responsive web interfaces.</li>
+              <li>Led the design and implementation of interactive UI components and dashboards.</li>
+              <li>Optimized frontend performance for fast load times and a seamless user experience.</li>
+              <li>Participated in Agile development, contributing to sprint planning and code reviews.</li>
+              <li>Developed custom, client-specific features aligning with business goals.</li>
+            </ul>
+          </div>
+        </div>
+        <div className="col"> 
+          <div className="experience-item">
             <h3 className="experience-role">TECHNICAL MENTOR</h3>
             <p className="experience-company">WeThinkCode_</p>
             <p className="experience-dates">Sept, 2022 - Dec, 2022</p>
             <ul className="experience-responsibilities">
-              <li>Developed and maintained user interfaces using React.js and Next.js.</li>
-              <li>Collaborated with designers to implement responsive designs.</li>
-              <li>Optimized application performance and ensured cross-browser compatibility.</li>
-            </ul>
-          </div>
-        </div>
-        <div className="col">
-          <div className="experience-item">
-            <h3 className="experience-role">FRONTEND DEVELOPER INTERN</h3>
-            <p className="experience-company">WeThinkCode_</p>
-            <p className="experience-dates">Jan, 2022 - Dec, 2022</p>
-            <ul className="experience-responsibilities">
-              <li>Assisted in the development of user interfaces using React.js.</li>
-              <li>Participated in code reviews and contributed to team knowledge sharing.</li>
-              <li>Helped troubleshoot and resolve frontend issues.</li>
-            </ul>
-          </div>
-        </div>
-        <div className="col">
-          <div className="experience-item">
-            <h3 className="experience-role">FRONTEND DEVELOPER INTERN</h3>
-            <p className="experience-company">WeThinkCode_</p>
-            <p className="experience-dates">Jan, 2022 - Dec, 2022</p>
-            <ul className="experience-responsibilities">
-              <li>Assisted in the development of user interfaces using React.js.</li>
-              <li>Participated in code reviews and contributed to team knowledge sharing.</li>
-              <li>Helped troubleshoot and resolve frontend issues.</li>
+              <li>Mentored junior students in software development and debugging.</li>
+              <li>Conducted code reviews and provided constructive feedback.</li>
+              <li>Facilitated workshops and peer-to-peer learning sessions.</li>
+              <li>Developed leadership and communication skills while guiding students.</li>
+              <li>Balanced mentorship duties with a rigorous software engineering curriculum.</li>
+              <li>Promoted independent problem-solving by guiding students through challenges.</li>
             </ul>
           </div>
         </div>
--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -4,8 +4,93 @@ import { Button } from '@/app/components/button';
 
 const Skills = () => {
   return (
-    <section className="container-fluid about-container">
-
+    <section className="container-fluid skills-container">
+      <div className="row">
+        <div className="col">
+          <h2 className="section-title skills-title">SKILLS</h2>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col skills-item">
+            <div className="row">
+              <div className="col">
+                <h3 className="skills-category">Tech Stack</h3>
+              </div>
+            </div>
+            <div className="row skills-row">
+              <div className="col">
+                <img src="/images/git.png" alt="Tech Stack" className='skills-icon'/>
+              </div>
+              <div className="col">
+                <img src="/images/visual-studio.png" alt="Tech Stack" className='skills-icon'/>
+              </div>
+              <div className="col">
+                <img src="/images/react.png" alt="Tech Stack" className='skills-icon'/>
+              </div>
+            </div>
+            <div className="row skills-row">
+              <div className="col">
+                <img src="/images/html.png" alt="Tech Stack" className='skills-icon'/>
+              </div>
+              <div className="col">
+                <img src="/images/css.png" alt="Tech Stack" className='skills-icon'/>
+              </div>
+              <div className="col">
+                <img src="/images/js.png" alt="Tech Stack" className='skills-icon'/>
+              </div>
+            </div>
+            <div className="row skills-row">
+              <div className="col">
+                <img src="/images/figma.png" alt="Tech Stack" className='skills-icon'/>
+              </div>
+              <div className="col">
+                <img src="/images/programming-language.png" alt="Tech Stack" className='skills-icon'/>
+              </div>
+              <div className="col">
+                <img src="/images/blockchain.png" alt="Tech Stack" className='skills-icon'/>
+              </div>
+            </div>
+        </div>
+        <div className="col skills-item">
+          <div className="row">
+            <div className="col">
+              <h3 className="skills-category">Soft Skills</h3>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <p className='soft-skill'>Leadership</p>
+            </div>
+            <div className="col">
+              <p className='soft-skill'>Communication</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <p className='soft-skill'>Critical Thinking</p>
+            </div>
+            <div className="col">
+              <p className='soft-skill'>Mentorship</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <p className='soft-skill'>Adaptability</p>
+            </div>
+            <div className="col">
+              <p className='soft-skill'>Creativity</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <p className='soft-skill'>Time Management</p>
+            </div>
+            <div className="col">
+              <p className='soft-skill'>Collaboration</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
